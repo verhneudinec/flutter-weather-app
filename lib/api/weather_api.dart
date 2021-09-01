f4f342b5c958628d488ed609b2df619e56e5b19d
Igor` <webarts@yandex.ru>
2021-09-01 23:49:27 +0800

    Добавлен метод для запроса данных о погоде

---
+++ lib/api/weather_api.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:flutter_weather_app/model/weather_forecast.dart';
+import 'package:flutter_weather_app/utilities/constants.dart';
+import 'package:http/http.dart' as http;
+
+class WeatherApi {
+  Future<WeatherForecast> fetchWeatherForecastWithCity({
+    String cityName,
+  }) async {
+    var queryParameters = {
+      'APPID': Constants.WEATHER_APP_ID,
+      'units': 'metric',
+      'q': cityName,
+    };
+
+    var uri = Uri.https(Constants.WEATHER_BASE_URL_DOMAIN,
+        Constants.WEATHER_FORECAST_PATH, queryParameters);
+
+    log("request: ${uri.toString()}");
+
+    var response = await http.get(uri);
+
+    print(response.body);
+
+    if (response.statusCode == 200) {
+      return WeatherForecast.fromJson(
+        json.decode(response.body),
+      );
+    } else {
+      throw Exception('Error  response');
+    }
+  }
+}
